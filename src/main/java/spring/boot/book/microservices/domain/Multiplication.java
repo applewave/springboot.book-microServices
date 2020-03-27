74148d2a6895530dde51ac9d3c4beeef4aac642e
OhJong <wonderfulstory5@gmail.com>
2020-03-27 22:34:20 +0900

    add : lombok

--- src/main/java/spring/boot/book/microservices/domain/Multiplication.java
+++ src/main/java/spring/boot/book/microservices/domain/Multiplication.java
@@ -1,35 +1,46 @@
 package spring.boot.book.microservices.domain;
 
-public class Multiplication {
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
-    private int factorA;
-    private int factorB;
+@RequiredArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+public final class Multiplication {
 
-    private int result;
+    private final int factorA;
+    private final int factorB;
 
-    public Multiplication(int factorA, int factorB) {
-        this.factorA = factorA;
-        this.factorB = factorB;
-        this.result = factorA * factorB;
+    Multiplication() {
+        this(0, 0);
     }
+//    private int result;
+//    public Multiplication(int factorA, int factorB) {
+//        this.factorA = factorA;
+//        this.factorB = factorB;
+//        this.result = factorA * factorB;
+//    }
 
-    public int getFactorA() {
-        return factorA;
-    }
+//    public int getFactorA() {
+//        return factorA;
+//    }
 
-    public int getFactorB() {
-        return factorB;
-    }
+//    public int getFactorB() {
+//        return factorB;
+//    }
 
-    public int getResult() {
-        return result;
-    }
+//    public int getResult() {
+//        return result;
+//    }
 
-    public String toString() {
-        return "Multiplication {" +
-                "factorA=" + factorA +
-                ", factorB=" + factorB +
-                ", result(A*B)=" + result +
-                "}";
-    }
+//    public String toString() {
+//        return "Multiplication {" +
+//                "factorA=" + factorA +
+//                ", factorB=" + factorB +
+//                ", result(A*B)=" + result +
+//                "}";
+//    }
 }
